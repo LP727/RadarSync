34ab23dedafdd219e796eade8aceaf362f1653f4
LP727 <lpbeliveau@protonmail.com>
2023-07-09 14:28:09 -0400

    Logo resources added, started the NavGraph and the Position Entity to be interfaceable with json objects but nothing is functionnal currently

---
+++ app/src/main/java/com/example/radarsync/data/PositionEntity.kt
@@ -0,0 +1,16 @@
+package com.example.radarsync.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
+
+@Entity(tableName = "position")
+data class PositionEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: String,
+    @Json(name = "lat") val latitude: Double,
+    @Json(name = "lon") val longitude: Double,
+    @Json(name = "alt") val altitude: Double,
+    val name: String,
+    @Json(name = "tim") val time: Long,
+)
